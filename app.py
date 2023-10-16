be73d877a73aeb1ff4f97e0b65d22f94ba3bda0d
Mackenzie Zappe <73668826+mackenzieZ@users.noreply.github.com>
2023-10-15 17:05:21 -0700

    testing git

---
+++ app.py
@@ -0,0 +1,130 @@
+
+from flask import Flask, request, jsonify, render_template
+import ast
+import numpy as np
+# Create the app object
+app = Flask(__name__)
+
+
+# importing function for calculations
+from basic_calculator_function import basic_calculator
+from basic_calculator_function import strat_cost_calc
+from basic_calculator_function import prob_success_attack
+from basic_calculator_function import calc_attacker_payoff
+from basic_calculator_function import calc_defender_payoff
+from basic_calculator_function import solve_game
+from basic_calculator_function import refactor_arrays_row
+from basic_calculator_function import refactor_arrays_col
+
+
+# Define calculator
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    #get cost values from form
+    active_cost_a = request.form['active_cost_a']
+    passive_cost_a = request.form['passive_cost_a']
+    defend_cost_d = request.form['defend_cost_d']
+    no_cost_d = request.form['no_cost_d']
+
+    costs_array = [float(active_cost_a),float(passive_cost_a),float(defend_cost_d),float(no_cost_d)]
+
+    #get effort values from form
+    active_effort_a = request.form['active_effort_a']
+    passive_effort_a = request.form['passive_effort_a']
+    defend_effort_d = request.form['defend_effort_d']
+    no_effort_d = request.form['no_effort_d']
+    
+    effort_array=[float(active_effort_a),float(passive_effort_a),float(defend_effort_d),float(no_effort_d)]
+    
+    #get attacker utilities from form
+    a11 = request.form['a11']
+    a12 = request.form['a12']
+    a21 = request.form['a21']
+    a22 = request.form['a22']
+
+    attacker_utility = [float(a11),float(a12),float(a21),float(a22)]
+    
+    #get defender utilities from form
+    d11 = request.form['d11']
+    d12 = request.form['d12']
+    d21 = request.form['d21']
+    d22 = request.form['d22']
+
+    defender_utility = [float(d11),float(d12),float(a21),float(a22)]
+    
+    calc_cost_array = []
+    #calculate costs for players
+    for x in range(len(costs_array)):
+        val = strat_cost_calc(effort_array[x], costs_array[x])
+        calc_cost_array.append(val)
+
+    print("calc_cost_arr " + str(calc_cost_array) )
+
+
+    #calculate probs of success attack
+    prob_success_array=[]
+    effort_array_a = [effort_array[0], effort_array[1]]
+    effort_array_d = [effort_array[2], effort_array[3]]
+    for x in range(len(effort_array_a)):
+        val= prob_success_attack(effort_array_a[x],effort_array_d[0])
+        prob_success_array.append(val)
+        val= prob_success_attack(effort_array_a[x],effort_array_d[1])
+        prob_success_array.append(val)
+
+    #calculate defender payoff
+    calc_d_payoff_array = []
+    calc_a_payoff_array = []
+    for x in range(len(defender_utility)):
+        print(x)
+        if (x % 2) == 0:
+            val = calc_defender_payoff(prob_success_array[x], defender_utility[x], calc_cost_array[2])
+            calc_d_payoff_array.append(val)
+                
+        else:
+            val = calc_defender_payoff(prob_success_array[x], defender_utility[x], calc_cost_array[3])
+            calc_d_payoff_array.append(val)
+    
+    for x in range(len(attacker_utility)):
+        print(x)
+        if (x <2):
+            val = calc_attacker_payoff(prob_success_array[x], attacker_utility[x], calc_cost_array[0])
+            calc_a_payoff_array.append(val)
+                
+        else:
+            val = calc_attacker_payoff(prob_success_array[x], attacker_utility[x], calc_cost_array[1])
+            calc_a_payoff_array.append(val)
+                
+                
+
+    
+    #result = basic_calculator(a,b,operation)
+
+    calc_list = [effort_array, calc_cost_array, prob_success_array, calc_a_payoff_array, calc_d_payoff_array]
+    result=1+2
+    return render_template('results.html', calc_list = calc_list )
+
+@app.route('/test',methods=['POST'])
+def results():
+
+    calc_list = request.form['selected_items']
+    calc_list_array = ast.literal_eval(calc_list)
+    attacker_payoffs = calc_list_array[3]
+    defender_payoffs = calc_list_array[4]
+
+    
+    rowPlayer = refactor_arrays_row(attacker_payoffs)
+    colPlayer = refactor_arrays_col(defender_payoffs)
+
+    solution = solve_game(rowPlayer, colPlayer)
+    
+    solution.append(attacker_payoffs)
+    solution.append(defender_payoffs)
+    return render_template('solution.html', solution=solution)
+
+if __name__ == "__main__":
+    app.run(debug=True) 
---
+++ basic_calculator_function.py
@@ -0,0 +1,105 @@
+import nashpy as nash
+import numpy as np
+import ast
+from sympy import symbols, Eq, solve
+
+#global vars
+decimal_len = 3
+
+def basic_calculator(a,b,operation):
+
+  if (a.isnumeric() & b.isnumeric()):
+    a=float(a)
+    b=float(b)
+    if operation == "add":
+      result = a + b
+    elif operation == "subtract":
+      result = a - b
+    elif operation == "divide":
+      result = a / b
+    elif operation == "multiply":
+      result = a * b
+    else:
+      result = "Operations supported: add, subtract, divide, multiple only"
+    
+  else:
+    result = "Please enter a valid number for a & b"
+    
+
+  return result
+
+def strat_cost_calc(effort, cost):
+
+  calc_cost = truncate_float((effort*effort)*cost,decimal_len)
+
+  return calc_cost 
+
+def prob_success_attack(e_a,e_d):
+  prob = truncate_float((2**(e_a-e_d))/2, decimal_len)
+
+  return prob
+
+def calc_defender_payoff(prob_success, utility, cost):
+  payoff = truncate_float(((1-prob_success)*utility) - cost, decimal_len)
+
+  return payoff
+
+def calc_attacker_payoff(prob_success, utility, cost):
+  payoff = truncate_float(((prob_success)*utility) - cost, decimal_len)
+
+  return payoff
+
+def truncate_float(float_number, decimal_places):
+    multiplier = 10 ** decimal_places
+    return int(float_number * multiplier) / multiplier
+
+
+def solve_game(payoffs_a, payoffs_d):
+  #payoffs_a = np.array([[4,0], [0,2]])
+  #payoffs_d = np.array([[2,0], [0,4]])
+
+  gameN = nash.Game(payoffs_a,payoffs_d)
+  gameN
+
+  equilibria = gameN.support_enumeration()
+  sol = []
+  for eq in equilibria:
+    sol.append(list(eq))
+
+  arr1 = sol[0]
+
+
+  #solve Utilities
+  sigma_r = np.array(arr1[0])
+  sigma_c = np.array(arr1[1])
+  utilities = gameN[sigma_r, sigma_c]
+
+  print("utilities for each player ")
+  print(utilities)
+
+  arr1.append(utilities)
+
+  for x in range(len(arr1)):
+    for y in range(len(arr1[x])):
+      print("before " + str(arr1[x][y]))
+      arr1[x][y] = truncate_float(arr1[x][y], decimal_len)
+      print("after " + str(arr1[x][y]))
+
+  return arr1
+
+
+def refactor_arrays_row(arr):
+  arr1 = [arr[0], arr[1]]
+  arr2 = [arr[2], arr[3]]
+  
+  newArr = [arr1, arr2]
+  print(newArr)
+  return newArr
+
+def refactor_arrays_col(arr):
+  arr1 = [arr[0], arr[2]]
+  arr2 = [arr[1], arr[3]]
+  
+  newArr = [arr1, arr2]
+  print(newArr)
+  return newArr
